faeb4104cdf3724e5b17b8035c88452a4fd79fa4
lexausart <garda985@Newschool.edu>
2020-12-19 15:14:28 -0500

    Made jQuery type slideshow for text

--- jquerylib.js
+++ jquerylib.js
@@ -1,3 +1,23 @@
 $(document).scroll(function(){
     $("#growing").animate({fontSize: '50px'});
 });
+
+$("#slide1").click(function() {
+    $(this).hide();
+    $("#slide2").show();
+});
+
+$("#slide2").click(function() {
+    $(this).hide();
+    $("#slide3").show();
+});
+
+$("#slide3").click(function() {
+    $(this).hide();
+    $("#slide4").show();
+});
+
+$("#slide4").click(function() {
+    $(this).hide();
+    $("#slide5").show();
+});
